9e7a5389fec2ecc647604a7641276b7e2cfb99a7
Enrique Rico <enriquesantosrico@gmail.com>
2017-02-08 15:56:35 -0800

    Fixed some grammar errors

--- app.js
+++ app.js
@@ -10,27 +10,25 @@ if(confirmQuiz === true) {
 var user = prompt('First question! What is your name?');
 console.log(user + '? ' + 'That\'s a pretty cool name!');
 
-alert('Okay! ' + user + ', first question!');
+alert('Okay ' + user + ', first question!');
 
-var quizResponseOne = prompt('Please Answer with Y or N. Does Enrique have any siblings?');
-
+var quizResponseOne = prompt( user + ' please answer with Y or N. Does Enrique have any siblings?');
 if(quizResponseOne.toUpperCase() === 'Y') {
   console.log('That is correct! Enrique has 4 brothers and 4 sisters.');
 } else {
-  console.log('Enrique is latino, so of course he has siblings! :P');
+  console.log('Enrique is latino, so of course he has siblings! :P Enrique has 4 brothers and 4 sisters.');
 }
 
 var quizResponseTwo = prompt('Once again please answer with Y or N. Does Enrique love dogs?');
-
 if(quizResponseTwo.toUpperCase() === 'Y') {
-  var inceptionQuestion = prompt('WAIT ARE YOU SURE HE DOES? Answer Y or N');
+  var inceptionQuestion = prompt('WAIT ARE YOU SURE HE DOES? Answer Y or N.');
   if(inceptionQuestion.toUpperCase() === 'Y') {
-    console.log('Hell yeah he does! Have you met him already?');
+    console.log('Hell yeah he does! ' + user + ' have you met Enrique already?');
   } else {
-    console.log('You\'re wrong! Not even close to being right. Learn more about him!');
+    console.log('You\'re wrong! Not even close to being right.' + user + ' Your should learn more about him!');
   }
 } else {
   console.log('You\'re wrong! Who doesn\'t love dogs!?');
 }
 
-alert('If you thought these questions were hard...... GET READY!');
+var questionResponseThree = prompt('Question #3. ' + user + '');
